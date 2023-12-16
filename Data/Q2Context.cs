5910832724a94a25dfc3963b06c42fe5ab57a6a0
danielwelikala <146511329+danielwelikala@users.noreply.github.com>
2023-12-17 05:07:08 +0530

    Add files via upload

---
+++ Data/Q2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Q2.Model;
+
+namespace Q2.Data
+{
+    public class Q2Context : DbContext
+    {
+        public Q2Context (DbContextOptions<Q2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Q2.Model.Students> Students { get; set; } = default!;
+
+        public DbSet<Q2.Model.Courses>? Courses { get; set; }
+    }
+}
